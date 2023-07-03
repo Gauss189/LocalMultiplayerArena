0ebfd337ae4cedf94f5b8ff8d8fefc31393cc216
Gauss189 <Gauss189@users.noreply.github.com>
2023-07-03 17:09:13 +0300

    Initial commit

---
+++ Assets/Scripts/CoinSpawnManager.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinSpawnManager : MonoBehaviour
+{
+    [SerializeField] private GameObject coinPrefab;
+    [SerializeField] private float spawnInterval = 7f;
+
+    private float minY = -4f;
+    private float maxY = 7f;
+    private float minX = -12f;
+    private float maxX = 12f;
+
+    private bool isSpawning = false;
+
+    private void Start()
+    {
+        isSpawning = true;
+        StartCoroutine(SpawnCoins());
+    }
+
+    private IEnumerator SpawnCoins()
+    {
+        while (isSpawning)
+        {
+            Vector3 spawnPosition = new Vector3(Random.Range(minX, maxX), Random.Range(minY, maxY), 0f);
+
+            // Check if the spawn position overlaps with a collider with tag "Wall"
+            Collider2D[] colliders = Physics2D.OverlapCircleAll(spawnPosition, 0.5f);
+            //
+            bool overlapsWall = false;
+            foreach (Collider2D collider in colliders)
+            {
+                if (collider.CompareTag("Wall"))
+                {
+                    overlapsWall = true;
+                    break;
+                }
+            }
+
+            if (overlapsWall)
+            {
+                continue;
+            }
+
+            Instantiate(coinPrefab, spawnPosition, Quaternion.identity);
+
+            yield return new WaitForSeconds(spawnInterval);
+        }
+    }
+
+ //  public void StopSpawning()
+ //  {
+ //      isSpawning = false;
+ //  }
+}
+
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,154 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody2D playerRb { get; private set; }
+
+    private Vector2 direction = Vector2.down;
+
+    [Header("Stats")]
+    public int score;
+    [SerializeField] private int health = 5;
+    public int currentHealth;
+
+
+
+    [Header("Controller")]
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private KeyCode inputUp = KeyCode.W;
+    [SerializeField] private KeyCode inputDown = KeyCode.S;
+    [SerializeField] private KeyCode inputLeft = KeyCode.A;
+    [SerializeField] private KeyCode inputRight = KeyCode.D;
+    [SerializeField] private KeyCode inputFire = KeyCode.Space;
+
+
+    [Header("Shooting")]
+    [SerializeField] private GameObject projectilePrefab;
+    [SerializeField] private float projectileSpeed;
+
+
+    [Header("AnimationSequnce")]
+    [SerializeField] private AnimatedSpriteRenderer spriteRendererUp;
+    [SerializeField] private AnimatedSpriteRenderer spriteRendererDown;
+    [SerializeField] private AnimatedSpriteRenderer spriteRendererLeft;
+    [SerializeField] private AnimatedSpriteRenderer spriteRendererRight;
+
+    private AnimatedSpriteRenderer activeSpriteRenderer;
+
+    private void Awake()
+    {
+        currentHealth = health;
+        playerRb = GetComponent<Rigidbody2D>();
+        activeSpriteRenderer = spriteRendererDown;
+    }
+
+    private void Update()
+    {
+        Vector2 currentDirection = direction;
+
+        if (Input.GetKeyDown(inputFire) && currentDirection != Vector2.zero)
+        {
+            Vector3 instantiatePosition = GetInstantiationPosition(currentDirection);
+            GameObject projectile = Instantiate(projectilePrefab, instantiatePosition, Quaternion.identity);
+            Rigidbody2D rb = projectile.GetComponent<Rigidbody2D>();
+            rb.velocity = currentDirection * projectileSpeed;
+        }
+        if (Input.GetKey(inputUp))
+        {
+            SetDirection(Vector2.up, spriteRendererUp);
+        }
+        else if (Input.GetKey(inputDown))
+        {
+            SetDirection(Vector2.down, spriteRendererDown);
+        }
+        else if (Input.GetKey(inputLeft))
+        {
+            SetDirection(Vector2.left, spriteRendererLeft);
+        }
+        else if (Input.GetKey(inputRight))
+        {
+            SetDirection(Vector2.right, spriteRendererRight);
+        }
+        else
+        {
+            SetDirection(Vector2.zero, activeSpriteRenderer);
+        }
+
+    }
+
+    private Vector3 GetInstantiationPosition(Vector2 direction)
+    {
+        Vector3 instantiatePosition = transform.position;
+
+        if (direction == Vector2.up)
+        {
+            instantiatePosition += new Vector3(0, 1, 0);
+        }
+        else if (direction == Vector2.down)
+        {
+            instantiatePosition += new Vector3(0, -1, 0);
+        }
+        else if (direction == Vector2.left)
+        {
+            instantiatePosition += new Vector3(-1, 0, 0);
+        }
+        else if (direction == Vector2.right)
+        {
+            instantiatePosition += new Vector3(1, 0, 0);
+        }
+
+        return instantiatePosition;
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 position = playerRb.position;
+        Vector2 translation = direction * speed * Time.fixedDeltaTime;
+
+        playerRb.MovePosition(position + translation);
+    }
+
+    private void SetDirection(Vector2 newDirection, AnimatedSpriteRenderer spriteRenderer)
+    {
+        direction = newDirection;
+
+        spriteRendererUp.enabled = spriteRenderer == spriteRendererUp;
+        spriteRendererDown.enabled = spriteRenderer == spriteRendererDown;
+        spriteRendererLeft.enabled = spriteRenderer == spriteRendererLeft;
+        spriteRendererRight.enabled = spriteRenderer == spriteRendererRight;
+
+        activeSpriteRenderer = spriteRenderer;
+        activeSpriteRenderer.idle = direction == Vector2.zero;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Projectile")
+        {
+            currentHealth--;
+            Destroy(collision.gameObject);
+        }
+        if (currentHealth <= 0)
+        {
+            Invoke(nameof(OnDeathSequence), 0.3f);
+            Destroy(collision.gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Coin")
+        {
+            score++;
+            Destroy(collision.gameObject);
+        }
+    }
+
+    private void OnDeathSequence()
+    {
+        gameObject.SetActive(false);
+        FindObjectOfType<GameManager>().CheckWinState();
+    }
+}
